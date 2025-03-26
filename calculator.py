2e9f45e7dcd872c3d4c6cba0237a703e37548e2a
AFTrust <aaronfeiner@gmail.com>
2025-03-26 15:31:37 -0700

    float

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-# python calculator and interactive mode
+# python calculator and interactive mode (running code directly in terminal)
 
 x = input("enter a value for x: ").strip()
 # function nesting: x = int(input("enter a value for x: "))
@@ -7,6 +7,6 @@ y = input("enter a value for y: ").strip()
 z = int(x) + int(y)
 # int is converting the string variable 'x' to an integer, so we can perform arithmetic operations on it
 
-print(z)    
+print(f"integer sum = {z}")    
 # if we wouldn't be using 'z' again, it is not necessary to store it in a variable, we can directly print the result
 # print(x + y) - if we used funciton nesting above to already convert strings to integer values 
---
+++ float.py
@@ -0,0 +1,10 @@
+
+x = float(input("Enter any number, x: "))
+y = float(input("Enter any number, y: "))
+
+z = x + y
+
+print(z)
+
+
+
